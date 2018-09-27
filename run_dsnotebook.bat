a8a6dd663e8a1c2bff0432bd200c51c3eb48f113
Michael A. Freitas, PhD <mike.freitas@gmail.com>
2018-09-27 13:29:06 -0400

    Update run_dsnotebook.bat

--- run_dsnotebook.bat
+++ run_dsnotebook.bat
@@ -1,7 +1,7 @@
 echo "Starting up Notebook"
  
 #EDIT THE FOLLOWING. CHANGE TO YOUR ORGS LICENSE_ID
-SET "docker_image=mmontgomery5/dsclub_jupyter_r341_py366"
+SET "docker_image=mfreitas/dsclub_jupyter_r341_py366"
  
 #EDIT THE FOLLOWING. CHANGE TO YOUR PREFERRED HTTP PORT
 SET port=8888
