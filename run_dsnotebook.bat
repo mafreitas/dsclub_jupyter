7e5fd3ebcaaa2ffb61a60c40168492b894f68f88
Michael A. Freitas, PhD <mike.freitas@gmail.com>
2018-09-27 13:28:53 -0400

    Update run_dsnotebook.bat

--- run_dsnotebook.bat
+++ run_dsnotebook.bat
@@ -1,20 +1,16 @@
-#!/usr/bin/env bash
 echo "Starting up Notebook"
-
+ 
 #EDIT THE FOLLOWING. CHANGE TO YOUR ORGS LICENSE_ID
-set "docker_image = mfreitas/dsclub_jupyter_r341_py366"
-
+SET "docker_image=mmontgomery5/dsclub_jupyter_r341_py366"
+ 
 #EDIT THE FOLLOWING. CHANGE TO YOUR PREFERRED HTTP PORT
-set port = 8888
-
-set "share = /c/Users/mfreitas/repos"
-set "docker_bin = c:\\Docker\\docker.exe"
-
-%docker_bin% run -d -p %port%:%port% -v "%share%":"/data" ^
---name notebookserver %docker_image%  /bin/bash -c ^
-"jupyter notebook --ip 0.0.0.0 --no-browser --allow-root ^
---NotebookApp.token='' --notebook-dir='/data'"
-
-sleep 2
-
+SET port=8888
+ 
+SET "share=/c/"
+SET "docker_bin=docker.exe"
+ 
+%docker_bin% run -d -p %port%:%port% -v "%share%":"/data" --name notebookserver %docker_image% /bin/bash -c "jupyter notebook --ip 0.0.0.0 --no-browser --allow-root --NotebookApp.token='' --notebook-dir='/data'"
+ 
+timeout 2
+ 
 start "" http://192.168.99.100:%port%
